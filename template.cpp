9c92b76a550797bba98f10468c04a94bdd0f56fe
Manish Joshi <manishjoshi394@gmail.com>
2018-09-13 23:13:04 +0530

    added my default template

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define SPEED ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FOR(i, a, b) for (ll i = a; i < b; ++i)
+#define RFOR(i, b, a) for (ll i = b; i >= a; --i)
+#define DEBUG(x) cout << ">> " << #x << " : " << x << endl;
+#define endl "\n"
+#define pb(x) push_back(x)
+#define ALL(x) x.begin(), x.end()
+#define mp(x, y) make_pair(x, y)
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll, ll>> vpll;
+typedef unordered_map<ll, ll> STll;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    SPEED
+
+}
