5f42efc397bc06ae7cf004cf3c8f3852e3b416d5
Manish Joshi <manishjoshi394@gmail.com>
2018-09-27 21:09:48 +0530

    Added Ordered_set alias

--- template.cpp
+++ template.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define SPEED ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define FOR(i, a, b) for (ll i = a; i < b; ++i)
 #define RFOR(i, b, a) for (ll i = b; i >= a; --i)
 #define DEBUG(x) cout << ">> " << #x << " : " << x << endl;
 #define endl "\n"
+#define F first
+#define S second
 #define pb(x) push_back(x)
 #define ALL(x) x.begin(), x.end()
 #define mp(x, y) make_pair(x, y)
 
 using namespace std;
 
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 typedef long long ll;
 typedef long double ld;
 typedef vector<long long> vll;
